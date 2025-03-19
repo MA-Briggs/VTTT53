335ece00dc27fc31418a9386efd4e29f881eeba3
pokemaz <98837743+MA-Briggs@users.noreply.github.com>
2025-03-19 18:45:15 +0000

    merge

---
+++ VTT53/Source/VTT53/VideoCallWidget.cpp
@@ -0,0 +1,137 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "VideoCallWidget.h"
+
+void UVideoCallWidget::SetupSDKEngine()
+{
+    if (IconImage != nullptr) {
+        agora::rtc::RtcEngineContext RtcEngineContext;
+        // Set App ID
+        RtcEngineContext.appId = TCHAR_TO_ANSI(*_appID);
+        // Set event handler
+        RtcEngineContext.eventHandler = this;
+        // Create and initialize RtcEngineProxy
+        RtcEngineProxy = agora::rtc::ue::AgoraUERtcEngine::Get();
+        RtcEngineProxy->initialize(RtcEngineContext);
+    }
+}
+
+void UVideoCallWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+    if (IconImage) {
+        IconImage->SetDesiredSizeOverride(FVector2D(128.f, 96.f));
+    }
+
+    SetupSDKEngine();
+}
+
+void UVideoCallWidget::NativeDestruct()
+{
+    Super::NativeDestruct();
+    // Clean up resources
+    if (RtcEngineProxy != nullptr && RtcEngineProxy->NativeRtcEnginePtr() != nullptr)
+    {
+        RtcEngineProxy->unregisterEventHandler(this);
+        RtcEngineProxy->Release();
+        //delete RtcEngineProxy;
+        RtcEngineProxy = nullptr;
+    }
+}
+
+void UVideoCallWidget::Join(int IN_UID)
+{
+    agora::rtc::ChannelMediaOptions options;
+    RtcEngineProxy->enableVideo();
+
+    // Automatically subscribe to all audio streams
+    options.autoSubscribeAudio = true;
+
+    options.autoSubscribeVideo = true;
+
+    // Publish the audio collected by the microphone
+    options.publishMicrophoneTrack = true;
+
+    options.publishCameraTrack = true;
+
+    // Set channel profile to live broadcasting
+    options.channelProfile = agora::CHANNEL_PROFILE_TYPE::CHANNEL_PROFILE_COMMUNICATION;
+    // Set user role to broadcaster
+    options.clientRoleType = agora::rtc::CLIENT_ROLE_TYPE::CLIENT_ROLE_BROADCASTER;
+    // Join the channel
+    RtcEngineProxy->joinChannel(TCHAR_TO_ANSI(_token), TCHAR_TO_ANSI(_channelName), IN_UID, options);
+}
+
+void UVideoCallWidget::Leave()
+{
+    RtcEngineProxy->leaveChannel();
+}
+
+void UVideoCallWidget::onLeaveChannel(const agora::rtc::RtcStats& stats)
+{
+    AsyncTask(ENamedThreads::GameThread, [=, this]()
+        {
+            agora::rtc::VideoCanvas videoCanvas;
+            videoCanvas.view = nullptr;
+            videoCanvas.uid = 0;
+            videoCanvas.sourceType = agora::rtc::VIDEO_SOURCE_TYPE::VIDEO_SOURCE_CAMERA;
+            RtcEngineProxy->setupLocalVideo(videoCanvas);
+        });
+}
+
+
+void UVideoCallWidget::onJoinChannelSuccess(const char* channel, agora::rtc::uid_t uid, int elapsed)
+{
+    if (this) {
+        AsyncTask(ENamedThreads::GameThread, [=, this]()
+            {
+                UE_LOG(LogTemp, Warning, TEXT("JoinChannelSuccess uid: %u"), uid);
+
+                agora::rtc::VideoCanvas videoCanvas;
+                videoCanvas.view = IconImage;
+                videoCanvas.uid = 0;
+                videoCanvas.sourceType = agora::rtc::VIDEO_SOURCE_TYPE::VIDEO_SOURCE_CAMERA;
+                RtcEngineProxy->setupLocalVideo(videoCanvas);
+            });
+    }
+    //UID = uid;
+}
+
+void UVideoCallWidget::onUserJoined(agora::rtc::uid_t uid, int elapsed)
+{
+    for (auto& Screen : CPP_Screens) {
+        if (uid == Cast<URemoteScreenWidget>(Screen->GetWidget())->UID) {
+            agora::rtc::VideoCanvas videoCanvas;
+            videoCanvas.view = Cast<URemoteScreenWidget>(Screen->GetWidget())->IconImage;
+            videoCanvas.uid = uid;
+            videoCanvas.sourceType = agora::rtc::VIDEO_SOURCE_TYPE::VIDEO_SOURCE_REMOTE;
+
+            agora::rtc::RtcConnection connection;
+            connection.channelId = TCHAR_TO_ANSI(_channelName);
+
+
+            ((agora::rtc::ue::AgoraUERtcEngine*)RtcEngineProxy)->setupRemoteVideoEx(videoCanvas, connection);
+
+        }
+    }
+}
+
+void UVideoCallWidget::onUserOffline(agora::rtc::uid_t uid, agora::rtc::USER_OFFLINE_REASON_TYPE reason)
+{
+    for (auto& Screen : CPP_Screens) {
+        if (uid == Cast<URemoteScreenWidget>(Screen->GetWidget())->UID) {
+            agora::rtc::VideoCanvas videoCanvas;
+            videoCanvas.view = nullptr;
+            videoCanvas.uid = uid;
+            videoCanvas.sourceType = agora::rtc::VIDEO_SOURCE_TYPE::VIDEO_SOURCE_REMOTE;
+
+            agora::rtc::RtcConnection connection;
+            connection.channelId = TCHAR_TO_ANSI(_channelName);
+
+            ((agora::rtc::ue::AgoraUERtcEngine*)RtcEngineProxy)->setupRemoteVideoEx(videoCanvas, connection);
+        }
+    }
+}
+
---
+++ VTT53/Source/VTT53/VideoCallWidget.h
@@ -0,0 +1,70 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/Image.h"
+#include "RemoteScreenWidget.h"
+#include <Components/WidgetComponent.h>
+
+#include "AgoraPluginInterface.h"
+
+#include "VideoCallWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VTT53_API UVideoCallWidget : public UUserWidget, public agora::rtc::IRtcEngineEventHandler
+{
+	GENERATED_BODY()
+protected:
+	// Initialize the custom Widget
+	virtual void NativeConstruct() override;
+public:
+
+	// Fill in your App ID
+	FString _appID = "4ee00587a6914d4ab6fde16ee511dff8";
+	// Fill in your channel name
+	const TCHAR* _channelName = _T("Channel1");
+	// Fill in Token
+	const TCHAR* _token = _T("007eJxTYPiYnSTCvLDekvPVtwj1TO39hzumZM5pmm7x+GeX/VW56XwKDCapqQYGphbmiWaWhiYpJolJZmkpqYZmqammhoYpaWkWuRaX0xsCGRlEd+xhZmSAQBCfg8E5IzEvLzXHkIEBAL8mIMg=");
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	int UID;
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget))
+	TObjectPtr<UImage> IconImage;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+	TArray<UWidgetComponent*> CPP_Screens;
+
+	UFUNCTION(BlueprintCallable)
+	void Join(int IN_UID);
+	UFUNCTION(BlueprintCallable)
+	void Leave();
+
+	agora::rtc::ue::AgoraUERtcEngine* RtcEngineProxy;
+	
+	// Callback triggered when the local user leaves the channel
+	void onLeaveChannel(const agora::rtc::RtcStats& stats) override;
+	
+	// Callback triggered when the local user successfully joins the channel
+	void onJoinChannelSuccess(const char* channel, agora::rtc::uid_t uid, int elapsed) override;
+
+	void onUserJoined(agora::rtc::uid_t uid, int elapsed) override;
+
+	void onUserOffline(agora::rtc::uid_t uid, agora::rtc::USER_OFFLINE_REASON_TYPE reason) override;
+
+
+	// Create and initialize IRtcEngine
+	void SetupSDKEngine();
+
+protected:
+	
+	// Clean up all session-related resources
+	void NativeDestruct() override;
+
+
+};
